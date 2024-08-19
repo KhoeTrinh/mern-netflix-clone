c841dad8a65c72a7e4506a3a4ecb5315bd8d5a16
KhoeTrinh <kelvintrinh09@gmail.com>
2024-08-19 15:43:27 +0700

    protect route middleware

---
+++ backend/middlewares/protectRoute.js
@@ -0,0 +1,47 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/authModals.js';
+import ENV_VARS from '../configs/envVars.js';
+
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies['jwt-netflix'];
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: 'No token, authorization denied',
+            });
+        }
+
+        const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({
+                success: false,
+                message: 'Token is invalid, authorization denied',
+            });
+        }
+
+        const user = await User.findById(decoded.userId).select(
+            '-password'
+        );
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: 'No user found, authorization denied',
+            });
+        }
+        req.user = user;
+
+        next();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: 'Server error, please try again later',
+        });
+    }
+};
+
+export default protectRoute;
--- backend/server.js
+++ backend/server.js
@@ -14,16 +14,20 @@ import connectDB from './configs/db.js';
 connectDB();
 
 // middleware
+import cookieParser from 'cookie-parser';
+import protectRoute from './middlewares/protectRoute.js';
+
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
 import authRoutes from './routes/authRoutes.js';
-import movieRoutes from './routes/movieRoutes.js'
-import tvRoutes from './routes/tvRoutes.js'
+import movieRoutes from './routes/movieRoutes.js';
+import tvRoutes from './routes/tvRoutes.js';
 
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/movie', movieRoutes);
-app.use('/api/v1/tv', tvRoutes);
+app.use('/api/v1/movie', protectRoute, movieRoutes);
+app.use('/api/v1/tv', protectRoute, tvRoutes);
 
 // app running on port
 app.listen(port, () => {
