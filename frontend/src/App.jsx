cf50c40d3b10c19d6e94fa6f35c361dff9623e76
KhoeTrinh <kelvintrinh09@gmail.com>
2024-08-21 15:55:11 +0700

    done search his page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
 import WatchPage from "./pages/WatchPage"
 import SearchPage from "./pages/SearchPage"
+import HistoryPage from "./pages/HistoryPage"
 
 
 function App() {
@@ -39,6 +40,7 @@ function App() {
         <Route path="/signup" element={!user ? <SignupPage /> : <Navigate to={'/'}/>}/>
         <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={'/login'}/>}/>
         <Route path="/search" element={user ? <SearchPage /> : <Navigate to={'/login'}/>}/>
+        <Route path="/history" element={user ? <HistoryPage /> : <Navigate to={'/login'}/>}/>
       </Routes>
       <Footer />
       <Toaster />
