6bdd1ce34fd6b48c7b77caba9218c5fa781641aa
KhoeTrinh <kelvintrinh09@gmail.com>
2024-09-16 09:07:40 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'https://mern-netflix-clone-2.onrender.com/',
+                changeOrigin: true,
+                secure: true,
             },
         },
     },
